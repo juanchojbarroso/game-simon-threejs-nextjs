a7c9745a0ceda3cfa2f538a6d8b19b74c8f4d7da
Juan Barroso <jbarroso@mymoid.com>
2022-08-20 10:04:08 +0100

    Add texture to floor

--- components/Floor.jsx
+++ components/Floor.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import * as THREE from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader.js";
+import { useLoader } from "@react-three/fiber";
 
 function Floor(props) {
+  const texture = useLoader(TextureLoader, "cloth.jpeg");
+  if (texture) {
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(2, 2);
+    texture.anisotropy = 16;
+  }
   return (
     <mesh {...props} recieveShadow={true}>
-      <boxBufferGeometry args={[20,1,10]} />
-      <meshPhysicalMaterial color='white' />
+      <boxBufferGeometry attach="geometry" args={[20, 1, 10]} />
+      {texture && <meshPhongMaterial attach="material" map={texture} />}
     </mesh>
   );
 }
