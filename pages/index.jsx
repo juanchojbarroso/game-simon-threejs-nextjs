67d5f712f07563b479e62f6baf96f235132cc9fc
Juan Barroso <jbarroso@mymoid.com>
2022-08-20 17:10:21 +0100

    Generate and validate sequence

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,44 +1,151 @@
+import React, { useState, useRef, Suspense, useEffect } from "react";
 import css from "../styles/Home.module.css";
 import { Canvas } from "@react-three/fiber";
-import Box from "../components/Box";
+import RandomObject from "../components/RandomObject";
 import OrbitControls from "../components/OrbitControls";
 import Light from "../components/Light";
 import Floor from "../components/Floor";
-import { Suspense } from "react";
 import { randomColor } from "../utils/colors";
 import { getRandomObject } from "../utils/objets3D";
 
 const ItemsPositionEasy = [
-  [-6, 2, 0],
-  [-2, 3, 0],
-  [2, 2, 0],
-  [6, 3, 0],
+  { position: [-6, 2, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [-2, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [2, 2, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [6, 3, 0], object3D: getRandomObject(), color: randomColor() }
 ];
 const ItemsPositionMedium = [
-  [-6, 3, 0],
-  [-3, 2, 0],
-  [0, 3, 0],
-  [3, 2, 0],
-  [6, 3, 0],
+  { position: [-6, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [-3, 2, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [0, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [3, 2, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [6, 3, 0], object3D: getRandomObject(), color: randomColor() }
 ];
 const ItemsPositionHard = [
-  [-9, 3, 0],
-  [-5.5, 3, 0],
-  [-2, 3, 0],
-  [1.5, 3, 0],
-  [5.5, 3, 0],
-  [9, 3, 0],
+  { position: [-9, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [-5.5, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [-2, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [1.5, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [5.5, 3, 0], object3D: getRandomObject(), color: randomColor() },
+  { position: [9, 3, 0], object3D: getRandomObject(), color: randomColor() }
 ];
 
 const fov = 80;
-const aspect = 3;
-const near = 4;
-const lookAt = 100;
+
+const difficultylevels = {
+  EASY: "EASY",
+  MEDIUM: "MEDIUM",
+  HARD: "HARD",
+};
+
+const difficultylevelsPositions = {
+  EASY: ItemsPositionEasy,
+  MEDIUM: ItemsPositionMedium,
+  HARD: ItemsPositionHard,
+};
 
 export default function Home() {
+  const [difficultyLevel, setDifficultyLevel] = useState(difficultylevels.EASY);
+  const [sequence, setSequence] = useState([]);
+  const [clickCounter, setClickCounter] = useState(0);
+
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+
+  useEffect(() => {
+    createSequence();
+
+    return () => {};
+  }, [difficultyLevel]);
+
+  function addToRefs(elementRef) {
+    if (elementRef && !revealRefs.current.includes(elementRef)) {
+      revealRefs.current.push(elementRef);
+    }
+  }
+
+  function createSequence() {
+    const sequenceLength = getCurrentDifficultyLevel().length;
+    const sequence = Array(sequenceLength)
+      .fill()
+      .map(() => getRandomInt(sequenceLength));
+    console.log(`sequence: ${sequence}`);
+    setSequence(sequence);
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function getIdFromKey(key) {
+    return key.split("_").pop();
+  }
+
+  function performSequence() {
+    console.log("performClick");
+    sequence.forEach((turn, i) => {
+      setTimeout(() => {
+        revealRefs.current[turn]?.performClickFromExternalSign();
+      }, i * 1000);
+    });
+  }
+
+  function validateSequence(objectId) {
+    debugger;
+    if (`${sequence[clickCounter]}` === getIdFromKey(objectId)) {
+      console.log("YUJU you pick the correct");
+      setClickCounter(clickCounter + 1);
+    } else {
+      console.log("BUUU you lose");
+      setClickCounter(0);
+    }
+  }
+
+  function handleObjectClick(objectId) {
+    validateSequence(objectId);
+  }
+
+  function getCurrentDifficultyLevel() {
+    return difficultylevelsPositions[difficultyLevel];
+  }
+
+  function updateDifficultyLevel(level) {
+    setDifficultyLevel(level);
+  }
+
   return (
     <>
-    <div style={{}}>Nivel: 1 </div>
+      <div style={{}}>
+        <>
+          <button
+            onClick={() => {
+              performSequence();
+            }}
+          >
+            Random
+          </button>
+          <button
+            onClick={() => {
+              updateDifficultyLevel(difficultylevels.EASY);
+            }}
+          >
+            EASY
+          </button>
+          <button
+            onClick={() => {
+              updateDifficultyLevel(difficultylevels.MEDIUM);
+            }}
+          >
+            MEDIUM
+          </button>
+          <button
+            onClick={() => {
+              updateDifficultyLevel(difficultylevels.HARD);
+            }}
+          >
+            HARD
+          </button>
+        </>
+      </div>
       <div className={css.scene}>
         <Canvas
           shadows={true}
@@ -48,14 +155,16 @@ export default function Home() {
           <ambientLight color={"white"} intensity={0.2} />
           <Light position={[0, 10, 0]} />
           <Suspense fallback={null}>
-            {ItemsPositionEasy.map((pos, index) => {
+            {getCurrentDifficultyLevel().map((item, index) => {
               return (
-                <Box
-                  key={`item-${index}`}
-                  itemId={`item-${index}`}
-                  _color={randomColor()}
-                  _object3D={getRandomObject()}
-                  position={pos}
+                <RandomObject
+                  itemRef={addToRefs}
+                  onObjectClick={handleObjectClick}
+                  key={`item_${index}`}
+                  itemId={`item_${index}`}
+                  _color={item.color}
+                  _object3D={item.object3D}
+                  position={item.position}
                 />
               );
             })}
